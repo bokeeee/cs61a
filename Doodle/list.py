7cf7fef322d14b4cea4123edc7ec418cecf198de
bokeeee <32112178+bokeeee@users.noreply.github.com>
2019-01-20 21:05:38 +0100

    Overdue commit

---
+++ Doodle/list.py
@@ -0,0 +1,21 @@
+def flatten(lst):
+    """Returns a flattened version of lst.
+
+    >>> flatten([1, 2, 3])     # normal list
+    [1, 2, 3]
+    >>> x = [1, [2, 3], 4]      # deep list
+    >>> flatten(x)
+    [1, 2, 3, 4]
+    >>> x = [[1, [1, 1]], 1, [1, 1]] # deep list
+    >>> flatten(x)
+    [1, 1, 1, 1, 1, 1]
+    """
+    "*** YOUR CODE HERE ***"
+    if type(lst) != list:
+        return [lst]
+    else:
+        return  sum([flatten(elem) for elem in lst], [])
+    
+
+    
+    
